727627e987c8a401a979e3f90d346985e7b47eed
Aneudy Mota <aneudy.motacatalino@outlook.com>
2021-03-01 19:25:52 -0400

    Initial commit

---
+++ Source/BuildingScape/WorldPosition.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Get object name
+	FString ObjectName =  GetOwner()->GetName();
+
+	// Get object position in the world
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+
+	// Log the object's name and position to the output log
+	UE_LOG(LogTemp, Warning, TEXT("%s's location in the world is: %s"), *ObjectName, *ObjectPosition);
+
+	
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
